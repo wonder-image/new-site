71e0102a43de27594f740d836a09845aa232c55b
Andrea Marinoni <marinoni@wonderimage.it>
2025-10-06 16:15:44 +0200

    Aggiornamento API

--- api/config.php
+++ api/config.php
@@ -4,8 +4,16 @@
     ini_set('display_errors', 0);
 
     header("Access-Control-Allow-Origin: *");
-    header("Accept: application/json");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Content-Type: application/json; charset=utf-8");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        
+        http_response_code(200);
+        exit(0);
+        
+    }
 
     $ROOT = str_replace('/api', "", __DIR__);
     require_once $ROOT."/vendor/wonder-image/app/wonder-image.php";
